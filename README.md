8a2c1d0c2c71337c27e742d47661e05549bfb8aa
vanessav02 <vanessah91@hotmail.com>
2022-05-05 12:05:50 -0300

    Hello World com spring boot

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
 # spring_boot_expert
-.idea
-*.iml
-*.class
-*.log
-.metadata
-bin/
-tmp/
-*.tmp
-*.bak
-*.swp
-.settings/
--- src/main/java/vanessav02/VendasApplication.java
+++ src/main/java/vanessav02/VendasApplication.java
@@ -2,14 +2,24 @@ package vanessav02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApplication {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
-
     }
 }
 
-//1. anotação para o spring reconhecer que é uma aplicação springboot
-//2. comando para inicializar a aplicação spring boot: .run(classe que vai inicializar a aplicação,args)
+//1. adiciona dependência do spring-boot-starter-web
+//2. anotação @RestController na classe - diz que é um controlador Rest, posso mandar mensagens para o browser através dessa classe
+//3. anotação @GetMapping no métódo - mapeando para que o browser entenda a URL "/hello" e mostre a mensagem retornada
+
